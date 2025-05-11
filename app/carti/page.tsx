9e0d097a9659ccdd49bce66bf0acb62f2ce16b08
petronel69 <66015530+petronel69@users.noreply.github.com>
2025-05-11 18:36:19 +0300

    Update page.tsx

--- app/carti/page.tsx
+++ app/carti/page.tsx
@@ -7,8 +7,7 @@ export default async function Page() {
   let clase = await getClase();
   return (
     <>
-    <h1>Acasa</h1>
-    <pre>{JSON.stringify(context.data, null, 2)}</pre>
+    <h1>Carti</h1>
     </>
   );
 }
