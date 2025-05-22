944ba11293800e8c98943d22ac7a30e69aede427
petronel69 <66015530+petronel69@users.noreply.github.com>
2025-05-22 18:34:37 +0300

    website title & primarybutton type fix

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
-export function PrimaryButton(props: {type?: "button" | "submit" | "reset", disabled?: boolean, text: ReactNode}) {
+export function PrimaryButton(props: {type?: "button" | "submit" | "reset", disabled?: boolean, text: ReactNode | string}) {
     return <button type={props.type} disabled={props.disabled} className="bg-blue-400 hover:bg-blue-300 hover:text-blue-600 hover:cursor-pointer text-gray-100 rounded-md shadow-sm border border-gray-200 mb-4 transition-all duration-200 ease-in-out px-4 py-2">{props.text}</button>;
 }
